f5ffc39e7448f86e5c1c317f2a9f08646d033dcd
indevsmail <36953836+indevsmail@users.noreply.github.com>
2023-08-15 07:01:46 +0000

    Git ignore changes

--- Easy/P4LongestCommonPrefix.cs
+++ Easy/P4LongestCommonPrefix.cs
@@ -10,7 +10,7 @@ namespace LeetCode.Easy
             for(int i=0; i<strs[0].Length;i++)
             {
                 char c=strs[0][i];
-                Console.WriteLine(strs[0][i]);
+                //Console.WriteLine(strs[0][i]);
                 for(int j=1;j<strs.Length;j++)
                 {                    
                     if(i>=strs[j].Length || c!=strs[j][i])
